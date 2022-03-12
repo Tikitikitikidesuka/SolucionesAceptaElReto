b615648c119e8a3280b08981d23362d8b1238a71
SneakyGerbil <deesneakygerbil@gmail.com>
2022-03-12 23:08:58 +0100

    Terminado 294 La Cuerda de la Cometa

--- 294LaCuerdaDeLaCometaFast.cpp
+++ 294LaCuerdaDeLaCometaFast.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <vector>
 
 
 #define MAX_INPUT 1000
 
-
-using std::vector;
 
 typedef std::int_fast64_t c_int;
 const c_int C_INT_MAX = INT64_MAX;
@@ -42,17 +39,28 @@ inline bool fastIntInputCheckEnd(c_int& number) {
     return true;
 }
 
+inline void fastIntOutput(c_int x){
+    std::int_fast8_t buffer[20];
+    register std::int_fast8_t i=0;
+    do{
+        buffer[i++] = (x % 10) + '0';
+        x /= 10;
+    } while(x);
+    i--;
+    while(i >= 0) putchar_unlocked(buffer[i--]);
+}
 
-c_int numFormas(c_int longitud, const vector<c_int>& longitudCuerdas) {
+
+c_int numFormas(c_int longitud, c_int numCuerdas, c_int longitudCuerdas[MAX_INPUT]) {
     c_int mat[2][MAX_INPUT + 1];
 
     for(int it = 1; it <= longitud; ++it)
         mat[0][it] = 0;
     
-    for(int it = 0; it <= longitudCuerdas.size(); ++it)
+    for(int it = 0; it <= numCuerdas; ++it)
         mat[it%2][0] = 1;
     
-    for(int it = 1; it <= longitudCuerdas.size(); ++it) {
+    for(int it = 1; it <= numCuerdas; ++it) {
         for(int l = 1; l <= longitud; ++l) {
             mat[it%2][l] = mat[(it-1)%2][l];
             if(l - longitudCuerdas[it-1] >= 0)
@@ -60,19 +68,19 @@ c_int numFormas(c_int longitud, const vector<c_int>& longitudCuerdas) {
         }
     }
 
-    return mat[longitudCuerdas.size()%2][longitud];
+    return mat[numCuerdas%2][longitud];
 }
 
-c_int minCuerdas(c_int longitud, const vector<c_int>& longitudCuerdas) {
+c_int minCuerdas(c_int longitud, c_int numCuerdas, c_int longitudCuerdas[MAX_INPUT]) {
         c_int mat[2][MAX_INPUT + 1];
 
         for(c_int it = 1; it <= longitud; ++it)
             mat[0][it] = C_INT_MAX;
 
-        for(c_int it = 0; it <= longitudCuerdas.size(); ++it)
+        for(c_int it = 0; it <= numCuerdas; ++it)
             mat[it%2][0] = 0;
 
-        for(c_int it = 1; it <= longitudCuerdas.size(); ++it) {
+        for(c_int it = 1; it <= numCuerdas; ++it) {
             for(c_int l = 1; l <= longitud; ++l) {
                 if(l - longitudCuerdas[it-1] >= 0 && mat[(it-1)%2][l - longitudCuerdas[it-1]] < C_INT_MAX) {
                     mat[it%2][l] = std::min(mat[(it-1)%2][l], 1 + mat[(it-1)%2][l - longitudCuerdas[it-1]]);
@@ -82,19 +90,19 @@ c_int minCuerdas(c_int longitud, const vector<c_int>& longitudCuerdas) {
             }
         }
 
-        return mat[longitudCuerdas.size()%2][longitud];
+        return mat[numCuerdas%2][longitud];
     }
 
-c_int costeMinimo(c_int longitud, const vector<c_int>& longitudCuerdas, const vector<c_int>& costeCuerdas) {
+c_int costeMinimo(c_int longitud, c_int numCuerdas, c_int longitudCuerdas[MAX_INPUT], c_int costeCuerdas[MAX_INPUT]) {
         c_int mat[2][MAX_INPUT + 1];
 
         for(c_int it = 1; it <= longitud; ++it)
             mat[0][it] = C_INT_MAX;
 
-        for(c_int it = 0; it <= longitudCuerdas.size(); ++it)
+        for(c_int it = 0; it <= numCuerdas; ++it)
             mat[it%2][0] = 0;
 
-        for(c_int it = 1; it <= longitudCuerdas.size(); ++it) {
+        for(c_int it = 1; it <= numCuerdas; ++it) {
             for(c_int l = 1; l <= longitud; ++l) {
                 if(l - longitudCuerdas[it-1] >= 0 && mat[(it-1)%2][l - longitudCuerdas[it-1]] < C_INT_MAX) {
                     mat[it%2][l] = std::min(mat[(it-1)%2][l], costeCuerdas[it-1] + mat[(it-1)%2][l - longitudCuerdas[it-1]]);
@@ -104,7 +112,7 @@ c_int costeMinimo(c_int longitud, const vector<c_int>& longitudCuerdas, const ve
             }
         }
 
-        return mat[longitudCuerdas.size()%2][longitud];
+        return mat[numCuerdas%2][longitud];
     }
 
 
@@ -112,28 +120,32 @@ int main() {
     c_int cuerdas, objetivo;
     while(fastIntInputCheckEnd(cuerdas)) {
         fastIntInput(objetivo);
-        vector<c_int> longCuerdas;
-        vector<c_int> costCuerdas;
-        longCuerdas.reserve(cuerdas);
-        costCuerdas.reserve(cuerdas);
+        c_int longCuerdas[MAX_INPUT];
+        c_int costCuerdas[MAX_INPUT];
 
-        while(cuerdas--) {
-            c_int inputLong, inputCost;
-            fastIntInput(inputLong);
-            fastIntInput(inputCost);
-            longCuerdas.push_back(inputLong);
-            costCuerdas.push_back(inputCost);
+        for(std::int_fast16_t cuerda = 0; cuerda < cuerdas; ++cuerda) {
+            fastIntInput(longCuerdas[cuerda]);
+            fastIntInput(costCuerdas[cuerda]);
         }
 
-        c_int formas = numFormas(objetivo, longCuerdas);
+        c_int formas = numFormas(objetivo, cuerdas, longCuerdas);
         if(formas > 0) {
-            std::cout << "SI " << formas << " " <<
-                minCuerdas(objetivo, longCuerdas) << " " <<
-                costeMinimo(objetivo, longCuerdas, costCuerdas);
+            putchar_unlocked('S');
+            putchar_unlocked('I');
+            putchar_unlocked(' ');
+
+            fastIntOutput(formas);
+            putchar_unlocked(' ');
+            
+            fastIntOutput(minCuerdas(objetivo, cuerdas, longCuerdas));
+            putchar_unlocked(' ');
+
+            fastIntOutput(costeMinimo(objetivo, cuerdas, longCuerdas, costCuerdas));
         } else {
-            std::cout << "NO";
+            putchar_unlocked('N');
+            putchar_unlocked('O');
         }
-        std::cout << "\n";
+        putchar_unlocked('\n');
     }
 
     return 0;
