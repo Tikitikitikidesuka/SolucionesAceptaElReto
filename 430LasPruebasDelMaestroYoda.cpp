47c9e5d0e72eceed4e1a53910dd343584ad37649
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-09-02 15:04:03 +0200

    Add 430 Las Pruebas del Maestro Yoda

---
+++ 430LasPruebasDelMaestroYoda.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+inline int intCeilDiv(int a, int b) {
+	return a / b + (a % b != 0);
+}
+
+int main() {
+	int pruebas = 0;
+
+	std::cin >> pruebas;
+	while(pruebas != 0) {
+		int prev = 0;
+
+		for(int prueba = 0; prueba < pruebas; ++prueba) {
+			int n = 0, m = 0;
+			std::cin >> n >> m;
+
+			if(n <= prev) {
+				int next = n + m * intCeilDiv(prev - n, m);
+				if(next == prev) next += m;
+				prev = next;
+			} else {
+				prev = n;
+			}
+		}
+
+		std::cout << prev << "\n";
+
+		std::cin >> pruebas;
+	}
+	
+	return 0;
+}
